dd45267f9c4223584c32e0941a4c17f7a3c97e20
tohieungoan <ngoanth.22it@vku.udn.vn>
2023-02-03 21:59:15 +0700

    trong nay la ghi chi ghi cm gi cung dc2

--- login/src/app/app.java
+++ login/src/app/app.java
@@ -6,7 +6,10 @@ public class app {
      //  new viewAdmin();
       // new teacherview();
   // new viewstudent();
-
+//
 }
-// thêm cai ghi chu nay để commit lại push lên
+// thêm cai ghi chu nay để commit lại push lên, rồi thao tác lại với tẻmianl  vscode đi e 2
+// la update ấy hả,um 
+// khi sua file thi commit se co so luong file changed
+// nhưng nảy làm ở file khác mà , có liên kết k, github sinh ra để làm việc nhóm, giả sử thằng khác xài laptop khác thì 
 }
