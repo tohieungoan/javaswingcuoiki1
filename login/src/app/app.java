0d8e81c27c20aa8106eccbb074376a4136c4e815
tohieungoan <ngoanth.22it@vku.udn.vn>
2023-02-03 22:07:29 +0700

    trong nay la ghi chi ghi cm gi cung d1c

--- login/src/app/app.java
+++ login/src/app/app.java
@@ -6,7 +6,7 @@ public class app {
      //  new viewAdmin();
       // new teacherview();
   // new viewstudent();
-//
+// nếu k  có gì thay đổi thì k dc commit hả, ko thay đổi file nao = có làm cme j đâu là pu2sh lên lại?=))
 }
 // thêm cai ghi chu nay để commit lại push lên, rồi thao tác lại với tẻmianl  vscode đi e 2
 // la update ấy hả,um 
